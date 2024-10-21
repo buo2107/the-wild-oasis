9f2940cb1d384cdf8d46fd11b4bbb153484eb21e
Atilas <vampire91145@gmail.com>
2024-10-22 00:01:53 +0800

    Converting the Modal to a Compound Component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,23 +1,49 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 import Modal from "../../ui/Modal";
 
+/* Convert the Modal to a Compound Component */
+// 因為 'isOpenModal' state 由 Modal 來管理比較合理，也更利於component reusability
 function AddCabin() {
-  const [isOpenModal, setisOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setisOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setisOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setisOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+
+      <Modal>
+        <Modal.Open opens="table">
+          <Button>See cabin table</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+          <CabinTable />
+        </Modal.Window>
+      </Modal>
+    </>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setisOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setisOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setisOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setisOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
